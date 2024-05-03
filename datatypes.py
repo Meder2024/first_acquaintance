4d132284f1c8efce1cd06206e7beb3264202ff2c
Meder2024 <kochebaev@gmail.com>
2024-05-03 17:51:38 +0600

    111

--- datatypes.py
+++ datatypes.py
@@ -1,17 +1,17 @@
-# Name = False
-# Name = 22
-# Name = 22.3
-# Name = "Meder"
-# Name = b"bytes"
-# Name = bytes([50,100,76,72,41])
-# print(chr(50))
-# Name = list('spisok')
-# print(Name)
-# Name = tuple('hello world')
-# print(Name)
-# Name = set('Hello')
-# print(Name)
-# d = {'first': 1, 'second': 2}
-# d = dict(first = 'first', second = 'second')
-# print(d)
-# print(type(d))
+Name = False
+Name = 22
+Name = 22.3
+Name = "Meder"
+Name = b"bytes"
+Name = bytes([50,100,76,72,41])
+print(chr(50))
+Name = list('spisok')
+print(Name)
+Name = tuple('hello world')
+print(Name)
+Name = set('Hello')
+print(Name)
+d = {'first': 1, 'second': 2}
+d = dict(first = 'first', second = 'second')
+pri
+print(type(d))
